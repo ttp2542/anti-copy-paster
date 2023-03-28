f153238c6fbfecaa0c8d2562acac3207f9abac33
ttp2542 <ttp2542@g.rit.edu>
2023-03-28 18:09:15 -0400

    FEAT-36 work-in-progress of the UserSettingsModel controller, hooking up frontend to backend

---
+++ src/main/java/org/jetbrains/research/anticopypaster/controller/CustomModelController.java
@@ -0,0 +1,54 @@
+package org.jetbrains.research.anticopypaster.controller;
+
+import org.jetbrains.research.anticopypaster.ide.ui.CustomMetricsModel;
+import org.jetbrains.research.anticopypaster.models.UserSettingsModel;
+
+/**
+ * Singleton instance since only one controller needs to exist.
+ */
+public final class CustomModelController {
+    private static final CustomModelController INSTANCE = new CustomModelController();
+    private UserSettingsModel userSettingsModel;
+
+    public CustomModelController(){
+
+    }
+
+    public static CustomModelController getInstance(){
+        return INSTANCE;
+    }
+
+    public void changeSettings(CustomMetricsModel customMetricsModel){
+        if(userSettingsModel != null) {
+            userSettingsModel.setComplexitySensitivity(parseSettingString(
+                    customMetricsModel.getComplexityDropdownValue())
+            );
+            userSettingsModel.setKeywordsSensitivity(parseSettingString(
+                    customMetricsModel.getKeywordsDropdownValue())
+            );
+            userSettingsModel.setSizeSensitivity(parseSettingString(
+                    customMetricsModel.getSizeDropdownValue())
+            );
+        }
+    }
+
+    /**
+     * Parses the settings string and converts to integer
+     * @param setting The string off/low/medium/high
+     * @return integer representation that CustomMetricsModel uses (0 - 3)
+     */
+    public int parseSettingString(String setting){
+        setting = setting.toLowerCase();
+        switch(setting){
+            case("high"):
+                return 3;
+            case("medium"):
+                return 2;
+            case("low"):
+                return 1;
+            // default case handles both "off" and improper formatting
+            default:
+                return 0;
+        }
+    }
+}
--- src/main/java/org/jetbrains/research/anticopypaster/ide/ui/CustomMetricsModel.java
+++ src/main/java/org/jetbrains/research/anticopypaster/ide/ui/CustomMetricsModel.java
@@ -9,4 +9,28 @@ public class CustomMetricsModel {
     String sizeCheckboxValue;
     String complexityDropdownValue;
     String complexityCheckboxValue;
+
+    public String getKeywordsDropdownValue(){
+        return this.keywordsDropdownValue;
+    }
+
+    public String getKeywordsCheckboxValue(){
+        return this.keywordsCheckboxValue;
+    }
+
+    public String getComplexityDropdownValue(){
+        return this.complexityDropdownValue;
+    }
+
+    public String getComplexityCheckboxValue(){
+        return this.complexityCheckboxValue;
+    }
+
+    public String getSizeDropdownValue(){
+        return this.sizeDropdownValue;
+    }
+
+    public String getSizeCheckboxValue(){
+        return this.sizeCheckboxValue;
+    }
 }
