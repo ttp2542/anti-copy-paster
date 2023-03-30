228ba8a24a00f410baf31d18678af4ab1f4bae67
Morgan West <mrw6863@rit.edu>
2023-03-30 18:00:49 -0400

    Remove checkboxes from model

--- src/main/java/org/jetbrains/research/anticopypaster/ide/ui/CustomMetricsModel.java
+++ src/main/java/org/jetbrains/research/anticopypaster/ide/ui/CustomMetricsModel.java
@@ -2,9 +2,6 @@ package org.jetbrains.research.anticopypaster.ide.ui;
 
 public class CustomMetricsModel {
     String keywordsDropdownValue;
-    String keywordsCheckboxValue;
     String sizeDropdownValue;
-    String sizeCheckboxValue;
     String complexityDropdownValue;
-    String complexityCheckboxValue;
 }
